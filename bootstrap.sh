6156b0ff36315155695aa18eee976370006b56bd
Kanowara Sinoka <maainnewkin59@gmail.com>
2026-01-21 07:46:24 +0900

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+JAR_URL="https://github.com/MCSRLauncher/Launcher/releases/latest/download/MCSRLauncher.jar"
+ICON_URL="https://avatars.githubusercontent.com/u/181990097?s=256"
+BUILD_SH_URL="https://raw.githubusercontent.com/sinokadev/MCSRLauncher-Linux/refs/heads/main/build.sh"
+
+DIST_DIR="./dist"
+ASSETS_DIR="./assets"
+
+mkdir -p "$DIST_DIR" "$ASSETS_DIR"
+
+need_cmd() {
+  command -v "$1" >/dev/null 2>&1 || {
+    echo "missing command: $1"
+    exit 1
+  }
+}
+
+need_cmd curl
+
+echo "[*] downloading latest MCSRLauncher.jar ..."
+
+if ! curl -fL -o "$DIST_DIR/MCSRLauncher.jar" "$JAR_URL"; then
+  echo "MCSRLauncher.jar not found in latest release"
+  exit 1
+fi
+
+echo "[+] MCSRLauncher.jar downloaded"
+
+echo "[*] downloading icon ..."
+curl -fL -o "$ASSETS_DIR/icon.png" "$ICON_URL"
+
+echo "[*] downloading build.sh ..."
+curl -fL -o "./build.sh" "$BUILD_SH_URL"
+chmod +x ./build.sh
+
+echo "[*] running build.sh ..."
+./build.sh
+
+echo "[+] done"
